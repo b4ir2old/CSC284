59768a366b66b050e1076d89cfd369b68dbe731a
AlexRussellGitHub <130182971+AlexRussellGitHub@users.noreply.github.com>
2024-02-02 19:38:07 -0500

    Update ParticipationList.java

--- ParticipationList.java
+++ ParticipationList.java
@@ -7,8 +7,7 @@ public class ParticipationList {
       ArrayList<Student> students = new ArrayList<>();
       
       //TODO: Create a Student Object with your first and last name and add them to the students list
-      students.add(new Student("Colson","Dunlap"));
-      students.add(new Student("John","Doe"));
+      students.add(new Student("Alex","Russell"));
      
       //Sort the list by last name
       students.sort((o1,o2) -> o1.getLastName().compareTo(o2.getLastName()));
